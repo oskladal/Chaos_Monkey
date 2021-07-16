c37866d3a8ee226b487d82384fde27f40af1c3d9
Ondřej Skládal <oskladal@csas.cz>
2021-07-16 15:20:05 +0200

    Vytvoření PHP skriptu, kontrolující zda je funkční spojení s DB

--- src/index.php
+++ src/index.php
@@ -1,15 +1,56 @@
    
 <?php 
+
+echo("<meta http-equiv='refresh' content='10'>");
+echo date('H:i:s Y-m-d');
+
+
+$username = 'root';
+$password = 'example';
+$servers = array(
+	'databaze' => 'test_ITRP' ,
+	'databaze2' => 'test_ITRP2',
+	'databaze3' => 'test_ITRP3',
+);
+
+
+foreach($servers as $server => $DB) {
+    
+	
+	// Create connection
+$conn = new mysqli($server, $username, $password, $DB);
+// Check connection
+if ($conn->connect_error) {
+   echo("<br>Connection failed: $DB " . $conn->connect_error);
+}
+
+else  echo "<br>Connected successfully: $DB";
+
+}
+
+
+
+
+/*
+echo("<meta http-equiv='refresh' content='10'>");
+echo date('H:i:s Y-m-d');
+   
+    
 // Create connection
 $conn = new mysqli("databaze", "root", "example","test_ITRP");
 
+
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+  die("<br>Připojení k databázi 1 se nezdařilo " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "<br>Připojení k databázi 1 je v pořádku" ;
+
+*/
 
 
+
+/*
 $sql = "INSERT INTO osoby (Jméno, Příjmení)
 VALUES ('LOREM', 'IPSUM');";
 $sql .= "INSERT INTO osoby (Jméno, Příjmení)
@@ -28,11 +69,8 @@ if ($conn->multi_query($sql) === TRUE) {
 
 
 
-
-
-
-
 $conn->close();
+*/
     
 ?>
     
